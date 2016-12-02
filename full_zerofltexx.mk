ee44d043268eb99a2a962709d9878507e6f06ec9
raj <x10forevers@users.noreply.github.com>
2016-12-01 21:38:34 -0400

    Update full_zerofltexx.mk

--- full_zerofltexx.mk
+++ full_zerofltexx.mk
@@ -25,7 +25,7 @@ PRODUCT_RUNTIMES := runtime_libart_default
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, device/samsung/zerofltexx/device.mk)
+$(call inherit-product, device/samsung/zeroltexx/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Set those variables here to overwrite the inherited values.
